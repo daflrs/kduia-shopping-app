237e88db3fbeb9c71c54d3db568eefa9764e62bb
daflrs <dangienew2010s@gmail.com>
2024-04-17 01:32:09 -0400

    finalize-cart-proj

--- src/App.js
+++ src/App.js
@@ -8,8 +8,31 @@ import Location from './components/Location';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <AppProvider>
+        <div class="container">
+                <h1 className='mt-3'>Shopping App</h1>
+            <div class="row mt-3">
+                <div class="col-sm">
+                    <CartValue />
+                </div>
+                <div class="col-sm">
+                    <Location />
+                </div>
+            </div>
+                <h3 className='mt-3'>Shopping Cart</h3>
+            <div class="row mt-3">
+                <div class="col-sm">
+                    <ExpenseList />
+                </div>
+            </div>
+                <h3 className='mt-3'>Add Items</h3>
+            <div class="row mt-3">
+                <div class="col-sm">
+                    <ItemSelected />
+                </div>
+            </div>
+        </div>
+    </AppProvider>
   );
 }
 
